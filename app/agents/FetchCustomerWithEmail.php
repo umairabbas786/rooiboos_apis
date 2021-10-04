db30de7944efff8dd2298605797e73f7c39f5fb8
umairabbas786 <umairabbass786@gmail.com>
2021-10-04 21:59:22 +0500

    magic

--- app/agents/FetchCustomerWithEmail.php
+++ app/agents/FetchCustomerWithEmail.php
@@ -19,20 +19,11 @@ class FetchCustomerWithEmail extends RooiBoosApi {
                 CustomerTableSchema::ID => $customer->getId(),
                 CustomerTableSchema::FIRST_NAME => $customer->getFirstName(),
                 CustomerTableSchema::LAST_NAME => $customer->getLastName(),
-                CustomerTableSchema::EMAIL => $customer->getEmail(),
-                CustomerTableSchema::PHONE_NUMBER => $customer->getPhoneNumber(),
-                CustomerTableSchema::PHONE_VERIFICATION => $customer->isPhoneVerified(),
-                CustomerTableSchema::PASSWORD => $customer->getPassword(),
                 CustomerTableSchema::CNIC_FRONT => $this->createLinkForCustomerCnicImage($customer->getCnicFront()),
                 CustomerTableSchema::CNIC_BACK => $this->createLinkForCustomerCnicImage($customer->getCnicBack()),
-                CustomerTableSchema::COUNTRY => $customer->getCountry(),
                 CustomerTableSchema::ACCOUNT_HOLDER_NAME => $customer->getAccountHolderName(),
                 CustomerTableSchema::ACCOUNT_NUMBER => $customer->getAccountNumber(),
-                CustomerTableSchema::IBAN_ACCOUNT_NUMBER => $customer->getIbanAccountNumber(),
-                CustomerTableSchema::ACCOUNT_TYPE => $customer->getAccountType(),
-                CustomerTableSchema::STATUS => $customer->isStatus(),
-                CustomerTableSchema::CREATED_AT => $customer->getCreatedAt(),
-                CustomerTableSchema::UPDATED_AT => $customer->getUpdatedAt()
+                CustomerTableSchema::IBAN_ACCOUNT_NUMBER => $customer->getIbanAccountNumber()
             ]
         ]);
     }
